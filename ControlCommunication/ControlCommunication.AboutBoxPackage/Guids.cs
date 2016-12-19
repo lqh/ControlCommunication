2439fa4e7fa28e5d0d07b5b783b6a2a1b4b21ed8
lqh <lqh.ls@163.com>
2016-12-19 09:09:15 +0800

    Add project files.

---
+++ ControlCommunication/ControlCommunication.AboutBoxPackage/Guids.cs
@@ -0,0 +1,14 @@
+﻿// Guids.cs
+// MUST match guids.h
+using System;
+
+namespace ControlCommunication.AboutBoxPackage
+{
+    static class GuidList
+    {
+        public const string guidAboutBoxPackagePkgString = "b7a9dd01-d4dd-484b-bdcc-1faef47dfc8c";
+        public const string guidAboutBoxPackageCmdSetString = "02c70864-c550-43d4-b2c2-c508352a1c7f";
+
+        public static readonly Guid guidAboutBoxPackageCmdSet = new Guid(guidAboutBoxPackageCmdSetString);
+    };
+}
